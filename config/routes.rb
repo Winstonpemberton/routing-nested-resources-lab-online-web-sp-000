c5348bd9fff09bdcb732eea20432cfa873e5199c
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-25 21:34:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :artists, only: [:show] do
+  resources :artists, only: [:show, :index] do
   # nested resource for posts
   resources :songs, only: [:show, :index]
 end
